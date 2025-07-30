21199666663125813fc32754f4d5341bb633c76e
ELITON JHONYS ALVES DA SILVA <20221101110011@ifrn.local>
2025-07-30 10:12:33 -0300

    etudos2

---
+++ estudos/e2/src/App.jsx
@@ -0,0 +1,13 @@
+import TrocaFundo from "./componentes/test1"
+import Formulario from "./componentes/test_2"
+import EffectAPI from "./componentes/test3"
+function App() {
+
+  return (
+      <div className="App">
+          <EffectAPI/>
+      </div>
+      )
+}
+
+export default App
